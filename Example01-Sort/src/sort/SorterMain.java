46ddc0558900d030328a430a2adf0ce80ccc3dac
LEE GyeongMin <min20@nhnnext.org>
2014-07-22 21:23:59 +0900

    change file path

--- Example01-Sort/src/sort/SorterMain.java
+++ Example01-Sort/src/sort/SorterMain.java
@@ -6,7 +6,7 @@ public class SorterMain {
 	static ISorter sorter = null;
 	static int[] NUMS = { 5, 3, 2, 1, 6, 3, 4, 0 };
 
-	static String DIR = "/Users/min/dev/SoftwareArchitecting/workspace/Example01-Sort/SortStrategy.xml";
+	static String DIR = "/Users/min/dev/SoftwareArchitecting/Eclipse workspace/Example01-Sort/SortStrategy.xml";
 
 	public static void main(String args[]) {
 		File file = new File(DIR);
